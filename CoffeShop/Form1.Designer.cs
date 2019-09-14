eb3d9e770262d8a6cb04f2442bf9670ff8a6ce3a
shafiqshuvo <shafiq.shuvo.nstu.ice@gmail.com>
2019-09-15 00:22:47 +0600

    update up to 15.09.2019

---
+++ CoffeShop/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace CoffeShop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.customernametext = new System.Windows.Forms.TextBox();
+            this.customernotext = new System.Windows.Forms.TextBox();
+            this.addresstext = new System.Windows.Forms.TextBox();
+            this.ordercombo = new System.Windows.Forms.ComboBox();
+            this.quantitycombo = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.customername = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customerno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.order = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quality = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.paytext = new System.Windows.Forms.TextBox();
+            this.Payment = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, -2);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Coffee Shop";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customer information";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 78);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Customer Name";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 116);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Customer No";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(38, 154);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Addresstext";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(38, 203);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Order";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(38, 246);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Quantity";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // customernametext
+            // 
+            this.customernametext.Location = new System.Drawing.Point(234, 78);
+            this.customernametext.Name = "customernametext";
+            this.customernametext.Size = new System.Drawing.Size(207, 26);
+            this.customernametext.TabIndex = 7;
+            // 
+            // customernotext
+            // 
+            this.customernotext.Location = new System.Drawing.Point(234, 116);
+            this.customernotext.Name = "customernotext";
+            this.customernotext.Size = new System.Drawing.Size(207, 26);
+            this.customernotext.TabIndex = 8;
+            // 
+            // addresstext
+            // 
+            this.addresstext.Location = new System.Drawing.Point(234, 148);
+            this.addresstext.Name = "addresstext";
+            this.addresstext.Size = new System.Drawing.Size(207, 26);
+            this.addresstext.TabIndex = 9;
+            // 
+            // ordercombo
+            // 
+            this.ordercombo.FormattingEnabled = true;
+            this.ordercombo.Items.AddRange(new object[] {
+            "Cold coffee",
+            "Hot coffee",
+            "Black coffee",
+            "Regular coffee"});
+            this.ordercombo.Location = new System.Drawing.Point(234, 200);
+            this.ordercombo.Name = "ordercombo";
+            this.ordercombo.Size = new System.Drawing.Size(207, 28);
+            this.ordercombo.TabIndex = 11;
+            this.ordercombo.SelectedIndexChanged += new System.EventHandler(this.ordercombo_SelectedIndexChanged);
+            // 
+            // quantitycombo
+            // 
+            this.quantitycombo.FormattingEnabled = true;
+            this.quantitycombo.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14"});
+            this.quantitycombo.Location = new System.Drawing.Point(234, 246);
+            this.quantitycombo.Name = "quantitycombo";
+            this.quantitycombo.Size = new System.Drawing.Size(207, 28);
+            this.quantitycombo.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.customername,
+            this.customerno,
+            this.address,
+            this.order,
+            this.quality});
+            this.dataGridView1.Location = new System.Drawing.Point(527, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(377, 196);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // customername
+            // 
+            this.customername.HeaderText = "Customer Name";
+            this.customername.Name = "customername";
+            // 
+            // customerno
+            // 
+            this.customerno.HeaderText = "Customer No";
+            this.customerno.Name = "customerno";
+            // 
+            // address
+            // 
+            this.address.HeaderText = "Address";
+            this.address.Name = "address";
+            // 
+            // order
+            // 
+            this.order.HeaderText = "Order";
+            this.order.Name = "order";
+            // 
+            // quality
+            // 
+            this.quality.HeaderText = "Quality";
+            this.quality.Name = "quality";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // paytext
+            // 
+            this.paytext.Location = new System.Drawing.Point(234, 297);
+            this.paytext.Name = "paytext";
+            this.paytext.Size = new System.Drawing.Size(100, 26);
+            this.paytext.TabIndex = 15;
+            // 
+            // Payment
+            // 
+            this.Payment.AutoSize = true;
+            this.Payment.Location = new System.Drawing.Point(42, 297);
+            this.Payment.Name = "Payment";
+            this.Payment.Size = new System.Drawing.Size(71, 20);
+            this.Payment.TabIndex = 16;
+            this.Payment.Text = "Payment";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 599);
+            this.Controls.Add(this.Payment);
+            this.Controls.Add(this.paytext);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.quantitycombo);
+            this.Controls.Add(this.ordercombo);
+            this.Controls.Add(this.addresstext);
+            this.Controls.Add(this.customernotext);
+            this.Controls.Add(this.customernametext);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox customernametext;
+        private System.Windows.Forms.TextBox customernotext;
+        private System.Windows.Forms.TextBox addresstext;
+        private System.Windows.Forms.ComboBox ordercombo;
+        private System.Windows.Forms.ComboBox quantitycombo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customername;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn order;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quality;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox paytext;
+        private System.Windows.Forms.Label Payment;
+    }
+}
+
---
+++ CoffeShop/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeShop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ordercombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ordercombo.SelectedItem.ToString() == "Cold coffee")
+            {
+                paytext.Text = (float.Parse(quantitycombo.Text) * 100).ToString();
+
+                dataGridView1.Rows.Add(customernametext.Text, customernotext.Text, addresstext.Text, ordercombo.Text, quantitycombo.Text, paytext.Text);
+            }
+            
+            else if (ordercombo.SelectedItem.ToString() == "Hot coffee")
+            {
+                paytext.Text = (float.Parse(quantitycombo.Text) * 90).ToString();
+
+                dataGridView1.Rows.Add(customernametext.Text, customernotext.Text, addresstext.Text, ordercombo.Text, quantitycombo.Text, paytext.Text);
+            }
+            else if (ordercombo.SelectedItem.ToString() == "Black coffee")
+            {
+                paytext.Text = (float.Parse(quantitycombo.Text) * 120).ToString();
+
+                dataGridView1.Rows.Add(customernametext.Text, customernotext.Text, addresstext.Text, ordercombo.Text, quantitycombo.Text, paytext.Text);
+            }
+            else if (ordercombo.SelectedItem.ToString() == "Regular coffee")
+            {
+                paytext.Text = (float.Parse(quantitycombo.Text) * 80).ToString();
+
+                dataGridView1.Rows.Add(customernametext.Text, customernotext.Text, addresstext.Text, ordercombo.Text, quantitycombo.Text, paytext.Text);
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
